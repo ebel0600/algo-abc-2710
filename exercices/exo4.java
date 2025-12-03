c0ded339909f032f6eeebe0a2d04548d4c773d84
ebel0600 <benjamin.beramis@gmail.com>
2025-12-03 10:59:29 +0100

    exo4

---
+++ exercices/exo4.java
@@ -0,0 +1,53 @@
+package exercices;
+
+import java.util.Scanner; 
+
+public class exo4 {
+
+    public static void main(String[] args) {
+        /* EXO5 : A l'aide de Scanner et Print, créer une équation  A multiplier par B diviser par C (tous types entiers) , récupérer le résultat et vérifier si elle inférieur ou supérieur à 10
+ * 
+ * 
+ * **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * equation = (EQUATION)
+ * 
+ * equation = 15
+ * 
+ * 
+ * equation est superieur
+ * 
+*/
+        // declaration de variables
+        float a;
+        float b;
+        float c;
+        Scanner entree = new Scanner(System.in);
+        float equation;
+
+        // saisie des valeurs
+        System.out.println("Sélectionner la valeur de A :");
+        a = entree.nextFloat();
+        System.out.println("Sélectionner la valeur de B :");
+        b = entree.nextFloat();
+        System.out.println("Sélectionner la valeur de C :");
+        c = entree.nextFloat();
+
+        //calcul
+        equation=a*b/c;
+
+        //presentation resultat
+        System.out.println("Le resultat de a*b/c vaut "+equation+".");
+
+        //condition de positionnement à 10
+        if(equation>10){
+            System.out.println("Le resultat est superieur a 10.");
+        }
+        else{
+            System.out.println("Le resultat est inferieur ou egal a 10.");
+        }
+
+        entree.close();
+    }
+
+}

7def46546d901356570a274b29c52db63ce3df7a
ebel0600 <benjamin.beramis@gmail.com>
2025-12-04 08:24:50 +0100

    xcopy = x

--- entrainement/x.java
+++ entrainement/x.java
@@ -1,22 +1,25 @@
 package entrainement;
 
+import java.util.Scanner;
+
 public class x {
     
     public static void main(String[] args) {
         //déclaration de variables
         char etoile = '*';
         char espace = ' ';
-        int marque = 1; // un marqueur pour indiquer quand il faut afficher une asterique
+        int marque = 0; // un marqueur pour indiquer quand il faut afficher une asterisque
+        int maximum; // pour établir la taille de la croix
+        Scanner entree = new Scanner(System.in);
+
+        System.out.println("Indiquez la taille de la croix");
+        maximum = entree.nextInt();
 
         //boucle des lignes
-        for(int i = 1; i<=5; i++){
+        for(int i = 0; i<maximum; i++){
             //boucle des colonnes
-            for(int j = 1; j<=5; j++){
-                //on distingue les cas ou le marqueur vaut 1 ou non pour ajuster la valeur du modulo
-                if(marque == 1 && (j-marque)%4==0){
-                    System.out.print(etoile);
-                }
-                else if(marque!=1 && j%marque==0){
+            for(int j = 0; j<maximum; j++){
+                if(j==marque || j==maximum-marque-1){
                     System.out.print(etoile);
                 }
                 else {
@@ -24,13 +27,9 @@ public class x {
                 }
             }
             //gestion de la valeur du marqueur en fonction des lignes
-            if(i<3){
-                marque++;
-            }
-            else{
-                marque--;
-            }
+            marque++;
             System.out.print("\n");
         }
+        entree.close();
     }
 }

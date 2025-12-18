fff6b378e742f45a7b1bd207de0bfaff8527dc42
ebel0600 <benjamin.beramis@gmail.com>
2025-12-18 09:12:45 +0100

    corrigé 2_7

--- exercices2/Exo2_7.java
+++ exercices2/Exo2_7.java
@@ -34,7 +34,7 @@ public class Exo2_7 {
                 switch (tabchaine.length) {
                     case 1:
                         // si on met une distance sans unite de mesure
-                        resultat = Double.parseDouble(chaineresult) / 1.609;
+                        resultat = Double.parseDouble(tabchaine[0]) / 1.609;
                         resultat *= 100;
                         conversion = Math.round(resultat);
                         resultat = conversion;
@@ -74,3 +74,66 @@ public class Exo2_7 {
 
     }
 }
+// corrigé
+/*
+ * package convertisseur_km_miles;
+ * 
+ * import java.util.Scanner;
+ * 
+ * public class ConvertisseurKmMi {
+ * 
+ * public static void main(String[] args) {
+ * // TO_DO Auto-generated method stub
+ * 
+ * System.out.println("Convertisseur Kilom�tre <-> Miles");
+ * System.out.println("----------------------------------");
+ * String saisie=" ";
+ * double distance;
+ * String [] montab;
+ * Scanner sc= new Scanner(System.in );
+ * System.out.
+ * print("Veuillez saisir une  distance � convertir avec un espace et l'unit� km pour kilom�tre ou mi pour miles :"
+ * );
+ * saisie=sc.nextLine();
+ * 
+ * montab= saisie.split(" ");
+ * distance=Double.parseDouble(montab[0]);
+ * String unite;
+ * if(montab.length<2)
+ * {
+ * unite="";
+ * 
+ * }else
+ * {
+ * unite=montab[1];
+ * }
+ * for( String el : montab)
+ * {
+ * 
+ * System.out.print(el+" ");
+ * 
+ * }
+ * /*
+ * for (int i = 0; i < montab.length; i++) {
+ * 
+ * System.out.print(montab[i]+ " ");
+ * 
+ * }
+ *
+ * if (unite.equals("km") || unite=="" ) {
+ * 
+ * double res= distance/1.609344 ;
+ * System.out.println("Cela donne en miles : "+res);
+ * 
+ * }else {
+ * 
+ * double res2= distance*1.609344 ;
+ * 
+ * System.out.println("Cela donne en km : "+res2);
+ * 
+ * 
+ * }
+ * }
+ * 
+ * }
+ */

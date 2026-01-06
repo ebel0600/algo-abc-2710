a2d3285f1ae3e36c786734dbc0ff32de20c8bab2
ebel0600 <benjamin.beramis@gmail.com>
2026-01-06 13:45:34 +0100

    exo2_7_2

--- exercices3/Exo2_7.java
+++ exercices3/Exo2_7.java
@@ -8,6 +8,8 @@ public class Exo2_7 {
         Scanner entree = new Scanner(System.in);
         int[] tabAge = new int[20];
         int jeunes = 0;
+        String djeuns="";
+        String adultes="";
 
         System.out.println("Entrez l'age de 20 personnes.");
         for (int i = 0; i < tabAge.length; i++) {
@@ -20,7 +22,25 @@ public class Exo2_7 {
             }
         }
         entree.close();
-        System.out.println("Parmi ces 20 personnnes, "+jeunes+" d'entres elles sont jeunes.\nProgramme terminé.");
+        if(jeunes == 20){
+            System.out.println("Toutes les personnes ont moins de 20 ans.");
+        }
+        else if(jeunes == 0){
+            System.out.println("Toutes les personnes ont plus de 20 ans.");
+        }
+        else{
+            for(int k = 0; k < tabAge.length; k++){
+                if(tabAge[k]<20){
+                    djeuns+=tabAge[k]+" ";
+                }
+                else{
+                    adultes+=tabAge[k]+" ";
+                }
+            }
+            System.out.println("Liste des personnes jeunes :\n"+djeuns+"\nListe de personnes non-jeunes :\n"+adultes);
+        }
+
+        System.out.println("Programme terminé");
 
     }
 }

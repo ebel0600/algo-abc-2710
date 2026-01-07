f59d80329a156e47a8f7d6c99b509dba7c0226f7
ebel0600 <benjamin.beramis@gmail.com>
2026-01-07 08:29:31 +0100

    exo3_4_reussi

--- exercices3/Exo3_4.java
+++ exercices3/Exo3_4.java
@@ -4,37 +4,43 @@ import java.util.Scanner;
 
 public class Exo3_4 {
     public static void main(String[] args) {
+        //declaration de variables
         int[] tableau;
         int taille;
         int permuteur;
         Scanner entree = new Scanner(System.in);
 
+        //parametrage du tableau
         System.out.println("Veuillez attribuer une taille de tableau");
         taille = entree.nextInt();
         entree.nextLine();
         tableau = new int[taille];
-
         System.out.println("Remplissez le tableau :");
-        for(int i = 0; i<tableau.length; i++){
-            System.out.println("en position "+i);
+        for (int i = 0; i < tableau.length; i++) {
+            System.out.println("en position " + i);
             tableau[i] = entree.nextInt();
             entree.nextLine();
         }
 
-        //recherche du plus petit élément
-        permuteur = tableau[0];
-        for(int j=0; j<tableau.length;j++){
-            if(permuteur > tableau[j]){
-                permuteur = tableau[j];
-                tableau[j] = tableau[0];
-                tableau[0] = permuteur;
+        // tri par ordre croissant
+        for (int k = 0; k < tableau.length; k++) {
+            for (int j = k; j < tableau.length; j++) {
+                if (tableau[k] > tableau[j]) {
+                    permuteur = tableau[j];
+                    tableau[j] = tableau[k];
+                    tableau[k] = permuteur;
+                }
+
             }
         }
 
+        //affichage du tableau
+        System.out.println("Voici le tri du tableau :");
         for (int l = 0; l < tableau.length; l++) {
             System.out.println(tableau[l]);
         }
 
+        entree.close();
     }
-    
+
 }

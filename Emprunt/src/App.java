536fb7dc9f80d2134c43b2ebf172e089c6015d05
ebel0600 <benjamin.beramis@gmail.com>
2026-01-08 09:57:05 +0100

    emprunt update

--- Emprunt/src/App.java
+++ Emprunt/src/App.java
@@ -6,18 +6,21 @@ public class App {
         Scanner sc = new Scanner(System.in);
         double capital;
         int nbmois;
+        String tabAmorti;
 
         System.out.println(" Veuiller saisir le capital à emprunter :");
         capital = sc.nextDouble();
         System.out.println("Veuiller saisir le taux d'intérêt annuel ");
         double tauxmensuel = (sc.nextDouble()) / 1200;
-        System.out.println("Veuiller saisir la durée dde remboursement (en années)");
+        System.out.println("Veuiller saisir la durée de remboursement (en années)");
         nbmois = (sc.nextInt()) * 12;
         double rembmensuel = App.calculMensualite(capital, nbmois, tauxmensuel);
 
         sc.close();
 
-        System.out.printf("Votre mensualité de rembourserment sera de %.2f Euros ", rembmensuel);
+        System.out.printf("Votre mensualité de rembourserment sera de %.2f Euros \n", rembmensuel);
+        tabAmorti = tabAmortissement(rembmensuel, nbmois, capital, tauxmensuel);
+        System.out.println("Voici votre tableau d'amortissement :\n"+tabAmorti);
 
     }
 
@@ -30,6 +33,28 @@ public class App {
         double mensualite = (_capital * _tauxmensuel) / Q;
 
         return mensualite;
+
+    }
+
+    private static String tabAmortissement (double mensualite, int nbMois, double capital, double tauxmensuel){
+        String resultat="Compteur mois\tPart interet\tPart capital\tCapital restant du\tMensualité\n";
+
+        double partInteret = capital * tauxmensuel;
+        double partCapital = mensualite - partInteret;
+
+        for (int i = 0; i <= nbMois; i++){
+            resultat += i+"\t\t"+arrondi(partInteret,1)+"\t\t"+arrondi(partCapital,1)+"\t\t"+Math.round(capital)+"\t\t\t"+Math.round(mensualite)+"\n";
+            capital-=partCapital;
+            partInteret = capital * tauxmensuel;
+            partCapital = mensualite-partInteret;
+        }
+
+        return resultat;
+    }
+
+    public static double arrondi(double nb, int nv) {
+
+        return (double) ((long) (nb * Math.pow(10, nv) + 0.5)) / Math.pow(10, nv);
 
     }
 }
